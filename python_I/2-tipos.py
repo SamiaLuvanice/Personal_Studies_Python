f0525181313cdfd8e58f72118437c9afa791bb13
Sâmia Luvanice <samia.luvanice.dev@gmail.com>
2026-02-24 16:42:21 -0300

    feat: start studying python

---
+++ python_I/2-tipos.py
@@ -0,0 +1,14 @@
+name = "Resident Evil 4 Remake"
+yearLauch = 2023
+gamePrice = 299.99
+planeIncluded = False
+
+print("Name:", name)
+print("Year of Launch:", yearLauch) 
+print("Game Price:", gamePrice)
+print("Plane Included:", planeIncluded)
+
+print(type(name))
+print(type(yearLauch))
+print(type(gamePrice))
+print(type(planeIncluded))
