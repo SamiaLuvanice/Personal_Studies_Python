d49e6f538cd45402e584ab6e56605584c9a3a011
Sâmia Luvanice <samia.luvanice.dev@gmail.com>
2026-03-22 15:06:12 -0300

    feat: start class about flask

---
+++ flask/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+conteudos = []
+registros = []
+
+@app.route('/', methods=['GET', 'POST'])
+def principal():
+    if request.method == 'POST':
+        if request.form.get('conteudo'):
+            conteudos.append(request.form.get('conteudo'))
+
+    return render_template(
+        'index.html',
+        conteudos=conteudos
+    )
+
+@app.route('/diario', methods=['GET', 'POST'])
+def diario():
+    if request.method == 'POST':
+        if request.form.get('aluno') and request.form.get('nota'):
+            aluno = request.form.get('aluno')
+            nota = request.form.get('nota')
+            registros.append(
+                {
+                    "aluno": aluno,
+                    "nota": nota
+                }
+            )
+
+    return render_template(
+        'sobre.html',
+        registros=registros
+    )
