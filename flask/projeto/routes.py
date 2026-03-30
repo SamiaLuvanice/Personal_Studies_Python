f0b4db7d517ab4bd1558f5250fefb4b9748751fe
Sâmia Luvanice <samia.luvanice.dev@gmail.com>
2026-03-30 16:29:30 -0300

    feat: update with pagination

--- flask/projeto/routes.py
+++ flask/projeto/routes.py
@@ -46,10 +46,15 @@ def lista_filmes(propriedade):
 
 @app.route('/livros')
 def lista_livros():
-    livros = Livro.query.all()
+    page = request.args.get('page', 1, type=int)
+    per_page = 2
+    todos_livros = Livro.query.paginate(
+        page=page, 
+        per_page=per_page
+        )
     return render_template(
         'livros.html',
-        livros=livros
+        livros=todos_livros
     )
 
 @app.route('/add_livro', methods=['GET', 'POST'])
